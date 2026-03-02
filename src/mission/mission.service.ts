6101744fa1e8044a12ec9dbeb65a951c4ede7bea
Lukazx15 <nattananpunpetch@gmail.com>
2026-03-02 19:26:30 +0700

    p01 v.7

--- src/mission/mission.service.ts
+++ src/mission/mission.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MissionService {
-  private readonly missions = [
+  private readonly missions: {
+    id: number;
+    codename: string;
+    status: 'ACTIVE' | 'COMPLETED' | 'FAILED';
+    }   [] = [
     { id: 1, codename: 'OPERATION_STORM', status: 'ACTIVE' },
     { id: 2, codename: 'SILENT_SNAKE', status: 'COMPLETED' },
     { id: 3, codename: 'RED_DAWN', status: 'FAILED' },
@@ -10,16 +14,19 @@ export class MissionService {
     { id: 5, codename: 'ECHO_FALLS', status: 'COMPLETED' },
     { id: 6, codename: 'GHOST_RIDER', status: 'COMPLETED' },
   ];
-    getSummary() {
-        const summary = {
-        ACTIVE: 0,
-        COMPLETED: 0,
-        FAILED: 0
+
+  getSummary(): Record<string, number> {
+    const summary: Record<string, number> = {
+      ACTIVE: 0,
+      COMPLETED: 0,
+      FAILED: 0,
     };
+
     for (let i = 0; i < this.missions.length; i++) {
-        const status = this.missions[i].status;
-            summary[status] = summary[status] + 1;
-        }
-    return summary;
+      const status = this.missions[i].status;
+      summary[status] += 1;
     }
+
+    return summary;
+  }
 }
